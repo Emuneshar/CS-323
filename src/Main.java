56bb8477756a65bfe1f50905933617437a8f66aa
Eshwar Muneshar <Emuneshar@gmail.com>
2022-01-09 18:30:55 -0500

    CS 323 Files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String [] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/Nums8.txt"));
+    }
+}
